b8e5a1ae3ae76ba62fa0fc24d286773870a62266
sandinm62 <sandinm62@live.com>
2023-06-16 09:37:48 +0300

    Added script HW1_1

--- HW1/HW1_1/main.py
+++ HW1/HW1_1/main.py
@@ -2,3 +2,13 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+number = int(input())
+summ = 0
+while number > 0:
+
+    remains = number % 10
+    summ += remains
+    number //= 10
+
+print(summ)
