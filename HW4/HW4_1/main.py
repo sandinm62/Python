fc7d40aa0e28f2d2bc480492ef526c3ee7ef9983
sandinm62 <sandinm62@live.com>
2023-06-27 16:48:08 +0300

    Fixed script about task4_1

--- HW4/HW4_1/main.py
+++ HW4/HW4_1/main.py
@@ -3,21 +3,46 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 # Пузырьковая сортировка топ !!!!)
-numericFirst = (8, 9, 9, 5, 5, 4, 7, 3)
-numericSeconde = (2, 1, 1, 1, 6, 4, 13)
-print(type(numericFirst))
-print(*numericFirst)
-print(type(numericSeconde))
-print(*numericSeconde)
-minValueFirstNumeric = numericFirst[0]
-minValueSecondeNumeric = numericSeconde[0]
-listFirst = []
 
-for i in range(numericFirst):    
-    if numericFirst[i] < minValueFirstNumeric:
-        minValueFirstNumeric = numericFirst
-    for j in range(numericSeconde):        
-        if minValueSecondeNumeric[j] < minValueSecondeNumeric:
-            minValueSecondeNumeric = numericSeconde[j]
+numericFirst = [8, 9, 9, 5, 5, 4, 7, 3]
+numericSeconde = [2, 1, 1, 1, 6, 4, 13]
+# print(type(numericFirst))
+numericFirst = tuple(set(sorted(numericFirst)))
+
+# print(type(numericFirst))
+
+print(*numericFirst)
+# print(type(numericSeconde))
+numericSeconde = tuple(set(sorted(numericSeconde)))
+print(*numericSeconde)
+countNumeric1 = 0
+countNumeric2 = 0
+
+
+for i in range(len(numericFirst) + len(numericSeconde)):
+    if countNumeric1 < len(numericFirst) and countNumeric2 < len(numericSeconde):
+        minValue1 = numericFirst[countNumeric1]
+        minValue2 = numericSeconde[countNumeric2]
+        if minValue1 < minValue2:
+            print(minValue1)
+            countNumeric1 += 1
+        elif minValue2 < minValue1:
+            print(minValue2)
+            countNumeric2 += 1
+        else:
+            print(minValue1)
+            countNumeric1 += 1
+            countNumeric2 += 1
+    elif countNumeric1 > len(numericFirst) and countNumeric2 < len(numericSeconde):
+        print(numericSeconde[countNumeric2])
+        countNumeric2 += 1
+    elif countNumeric1 < len(numericFirst) and countNumeric2 > len(numericSeconde):
+        print(numericFirst[countNumeric1])
+        countNumeric1 += 1
+
+    
+    
+
+
 
 
