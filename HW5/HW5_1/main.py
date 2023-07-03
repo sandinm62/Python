702a9b4dd51f2e9cd729c7f695fd203c96ca5847
sandinm62 <sandinm62@live.com>
2023-07-03 13:00:06 +0300

    Added first script task 5_1

---
+++ HW5/HW5_1/main.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+# *Пример:*
+# A = 3; B = 5 -> 243 (3⁵)
+# A = 2; B = 3 -> 8 
+
+def degree(a, b, count = 1, numDegree = 1):
+    
+    if count == b:
+        numDegree = numDegree * a
+        print(numDegree)
+        return numDegree
+    else:
+        numDegree = numDegree * a
+        # print(numDegree)
+        count += 1
+        degree(a, b, count, numDegree)
+
+
+number = int(input('Введите число которое будете возводить в степень: '))
+degreNumber = int(input('Введите степень числа: '))
+
+degree(number, degreNumber) 
+
+    
